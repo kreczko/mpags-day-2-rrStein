12ce4904403a6f11c80d5619d8767678084e4fb1
rrStein <rrstein521@gmail.com>
2019-10-25 13:57:35 +0100

    cleaned

--- edited_mpags-cipher.cpp
+++ edited_mpags-cipher.cpp
@@ -25,7 +25,6 @@ std::string transformChar(const char in_char){
   if (std::isalpha(in_char))
   {
     inputText = std::toupper(in_char);
-    // continue;
   }
 
   std::vector<std::string> digits{"ZERO","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE"};
@@ -47,41 +46,6 @@ std::string transformChar(const char in_char){
   std::map <char, std::string>::iterator it;
   it = my_map.find(in_char);
   inputText += my_map[in_char];
-
-//   // Transliterate digits to English words
-//   switch (in_char)
-//   {
-//   case '0':
-//     inputText += "ZERO";
-//     break;
-//   case '1':
-//     inputText += "ONE";
-//     break;
-//   case '2':
-//     inputText += "TWO";
-//     break;
-//   case '3':
-//     inputText += "THREE";
-//     break;
-//   case '4':
-//     inputText += "FOUR";
-//     break;
-//   case '5':
-//     inputText += "FIVE";
-//     break;
-//   case '6':
-//     inputText += "SIX";
-//     break;
-//   case '7':
-//     inputText += "SEVEN";
-//     break;
-//   case '8':
-//     inputText += "EIGHT";
-//     break;
-//   case '9':
-//     inputText += "NINE";
-//     break;
-//   }
 
   return inputText;
 }
