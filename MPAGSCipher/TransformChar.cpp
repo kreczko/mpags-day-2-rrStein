bd1ac0591606b3fc5836142ae74ee5cb8dfdfa3e
kreczko <lkreczko@googlemail.com>
2019-11-07 10:25:46 +0000

    uniform initialization of std::map (now it can be const)

--- MPAGSCipher/TransformChar.cpp
+++ MPAGSCipher/TransformChar.cpp
@@ -15,19 +15,20 @@ std::string transformChar(const char in_char) {
   // digits{"ZERO","ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE"};
 
   // Map input numbers to their corresponding words
-  std::map<char, std::string> my_map = {{'0', "ZERO"}};
+  const std::map<char, std::string> my_map = {
+    {'0', "ZERO"},
+    {'1', "ONE"},
+    {'2', "TWO"},
+    {'3', "THREE"},
+    {'4', "FOUR"},
+    {'5', "FIVE"},
+    {'6', "SIX"},
+    {'7', "SEVEN"},
+    {'8', "EIGHT"},
+    {'9', "NINE"},
+  };
 
-  my_map['1'] = "ONE";
-  my_map['2'] = "TWO";
-  my_map['3'] = "THREE";
-  my_map['4'] = "FOUR";
-  my_map['5'] = "FIVE";
-  my_map['6'] = "SIX";
-  my_map['7'] = "SEVEN";
-  my_map['8'] = "EIGHT";
-  my_map['9'] = "NINE";
-
-  inputText += my_map[in_char];
+  inputText += my_map.at(in_char);
 
   // If the character isn't alphabetic or numeric or space, DONT add it.
   // Our ciphers can only operate on alphabetic characters.
