ce11a7df142642a03f0ef09ce01c07f138ae12a7
kreczko <lkreczko@googlemail.com>
2019-11-07 14:34:14 +0000

    runCaesarCipher: for-each loop instead of counter

--- MPAGSCipher/RunCaesarCipher.cpp
+++ MPAGSCipher/RunCaesarCipher.cpp
@@ -7,7 +7,6 @@
 std::string runCaesarCipher(const std::string &text, const int &key,
                             const std::string &method) {
   std::string result{""};
-  int len{text.size()};
   int s{key};
 
   // "Reverse" the key if decryption is requested
@@ -15,10 +14,10 @@ std::string runCaesarCipher(const std::string &text, const int &key,
     s = 26 - key; // Based on 26 letter alphabet
   }
   // Loop through the whole text and apply transformation to each character
-  for (int i = 0; i < len; i++) {
+  for (const auto& letter: text) {
     // Encrypt Uppercase letters based on their ASCII codes (the +/- 65)
-    if (isupper(text[i])) {
-      result += char(int(text[i] + s - 65) % 26 + 65);
+    if (isupper(letter)) {
+      result += char(int(letter + s - 65) % 26 + 65);
 
       // Extra code for encrypting lowercase letters based on their ASCII codes
       // } else {
